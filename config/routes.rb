f49e70de824c3cf74fd5373618ec858ba8f03700
Miguel Vaello <miguel.vaellomartinez@gmail.com>
2013-12-21 18:05:16 +0100

    Añadiedno la ruta la página principal.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Gultalks::Application.routes.draw do
+  get "main/talks"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,6 +51,7 @@ Gultalks::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+    root :to => 'main#talks'
 
   # See how all your routes lay out with "rake routes"
 
