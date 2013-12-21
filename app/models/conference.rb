1925ce61f81b83681e34eabf4e5108493f7fc1d6
Samuel Rodriguez <samrs65@gmail.com>
2013-12-21 13:48:09 +0100

    Adding relationships

--- app/models/conference.rb
+++ app/models/conference.rb
@@ -1,3 +1,4 @@
 class Conference < ActiveRecord::Base
   attr_accessible :active, :call_for_papers_enabled, :call_for_papers_end_date, :call_for_papers_start_date, :coordinator, :creation_date, :description, :end_date, :start_date, :title, :voting_enabled, :voting_end_date, :voting_start_date
+  has_many :talks
 end
--- app/models/talk.rb
+++ app/models/talk.rb
@@ -1,3 +1,4 @@
 class Talk < ActiveRecord::Base
   attr_accessible :active, :assisted_by, :brief_description, :comments, :content_url, :creation_time, :day, :description, :end_time, :level, :room, :start_time, :tags, :teacher, :teacher_contact_info, :title, :votes
+  belongs_to :conference
 end
