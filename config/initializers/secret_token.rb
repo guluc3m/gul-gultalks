0ae3a54413e6132c9f490cc3618316d860a76404
Roberto Muñoz <roberto@gul.es>
2013-12-21 12:43:50 +0100

    Primer commit con las gemas instaladas, y la de mysql sin el gestor de paquetes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gultalks::Application.config.secret_token = '24592d544b14e86597d94f7d148496ef05a9943d625403abf83a9afe8c96ca046164dde891b42fe526bfb297573fd003d0a44d2cd18d654cb079c43b5602e117'
