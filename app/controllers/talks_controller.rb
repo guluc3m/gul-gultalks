6bf6d289be17d7b43a9620bca406223ac83a4f5e
Miguel Vaello <miguel.vaellomartinez@gmail.com>
2014-02-11 21:58:37 +0100

    Proposed controller as main controller for talks

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -1,5 +1,21 @@
 class TalksController < ApplicationController
+  
+  def index 
+    @talks = Talk.all
+  end
+
   def new
     @talk = Talk.new
   end
+
+  def show
+
+  end
+
+  def create
+      @talk = Talk.new()
+      @talk.save
+      redirect_to @thaks
+  end
+
 end
