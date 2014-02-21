40f3eaef61c5ceb19c5caba444e8e5ff98a58165
Miguel Vaello <miguel.vaellomartinez@gmail.com>
2014-02-21 13:16:15 +0100

    Fixed some types.
    - Some types were set to not null and it makes the app crash during adding talk process.
    - Added more default values.

--- db/migrate/20131221123535_create_talks.rb
+++ db/migrate/20131221123535_create_talks.rb
@@ -5,18 +5,18 @@ class CreateTalks < ActiveRecord::Migration
       t.string :brief_description, :null => false
       t.text :description, :limit => 2048, :null => false
       t.string :speaker, :limit => 64, :null => false
-      t.string :room, :null => false
+      t.string :room
       t.date :date
-      t.time :start_time, :null => false
-      t.time :end_time, :null => false
-      t.string :assisted_by, :null => false
+      t.time :start_time
+      t.time :end_time
+      t.string :assisted_by
       t.string :speaker_contact_info, :null => false
       t.integer :votes, :default => 0, :null => false
       t.text :comments, :limit => 2048
       t.integer :level, :null => false
       t.string :tags, :limit => 64
       t.string :content_url
-      t.boolean :active
+      t.boolean :active, :default => true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -31,22 +31,22 @@ ActiveRecord::Schema.define(version: 20131221124707) do
   end
 
   create_table "talks", force: true do |t|
-    t.string   "title",                limit: 128,              null: false
-    t.string   "brief_description",                             null: false
-    t.text     "description",          limit: 2048,             null: false
-    t.string   "speaker",              limit: 64,               null: false
-    t.string   "room",                                          null: false
+    t.string   "title",                limit: 128,                 null: false
+    t.string   "brief_description",                                null: false
+    t.text     "description",          limit: 2048,                null: false
+    t.string   "speaker",              limit: 64,                  null: false
+    t.string   "room"
     t.date     "date"
-    t.time     "start_time",                                    null: false
-    t.time     "end_time",                                      null: false
-    t.string   "assisted_by",                                   null: false
-    t.string   "speaker_contact_info",                          null: false
-    t.integer  "votes",                             default: 0, null: false
+    t.time     "start_time"
+    t.time     "end_time"
+    t.string   "assisted_by"
+    t.string   "speaker_contact_info",                             null: false
+    t.integer  "votes",                             default: 0,    null: false
     t.text     "comments",             limit: 2048
-    t.integer  "level",                                         null: false
+    t.integer  "level",                                            null: false
     t.string   "tags",                 limit: 64
     t.string   "content_url"
-    t.boolean  "active"
+    t.boolean  "active",                            default: true
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "conference_id"
