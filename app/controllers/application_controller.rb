b38ece1f92c664f3d336a7c67581d33f7d28b4cb
Rafael Medina <rafamedgar@gmail.com>
2014-06-02 19:57:11 +0200

    Added localization files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_locale
+
+  def set_locale
+    I18n.locale = http_accept_language.compatible_language_from(I18n.available_locales)
+  end
 end
