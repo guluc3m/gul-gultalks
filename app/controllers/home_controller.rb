461d4456bf6c7893df5e339ed523c0daca2caaa6
Rafael Medina <rafamedgar@gmail.com>
2014-06-02 12:34:22 +0200

    Adjusted condition checks

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   respond_to :html, :json, :xml
 
   def index
-    conference = Conference.find_by active: 1
+    conference = Conference.find_by active: true
     if conference
       redirect_to :controller => 'conferences', :action => 'show', :id => conference.id
     else
