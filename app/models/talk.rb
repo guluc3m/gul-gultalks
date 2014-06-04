3d84b76342467545865fa1875e3a540d49277269
Rafael Medina <rafamedgar@gmail.com>
2014-06-05 01:26:49 +0200

    Talk slugs should now be unique (per conference)

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -1,6 +1,13 @@
 class Talk < ActiveRecord::Base
   extend FriendlyId
- attr_accessible :active, :assisted_by, :brief_description, :comments, :conference_id, :content_url, :date, :description, :end_time, :level, :location, :room, :start_time, :speaker, :speaker_contact_info, :tags, :title, :votes
+ attr_accessible :active, :assisted_by, :brief_description, :comments, :conference_id, :content_url, :date, :description, :end_time, :id, :level, :location, :room, :start_time, :speaker, :speaker_contact_info, :tags, :title, :votes
   belongs_to :conference
-  friendly_id :title, :use => [:slugged, :scoped], :scope => :conference
+  friendly_id :slug_candidates, :use => [:slugged, :scoped], :scope => :conference
+
+  def slug_candidates
+    [
+      :title,
+      [:title, Talk.last.id+1]
+    ]
+  end
 end
