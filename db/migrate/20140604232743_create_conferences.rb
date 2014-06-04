8fc00560617274d58b7d367a0ab4c4c435f80deb
Rafael Medina <rafamedgar@gmail.com>
2014-06-05 01:29:08 +0200

    Minor fix in migration schemas

--- db/migrate/20140604232743_create_conferences.rb
+++ db/migrate/20140604232743_create_conferences.rb
@@ -3,7 +3,7 @@ class CreateConferences < ActiveRecord::Migration
     create_table :conferences do |t|
       t.string :title, :limit => 128, :null => false
       t.text :description, :limit => 1024, :null => false
-      t.text :location, :limit => 64, :null => false
+      t.string :location, :limit => 32, :null => false
       t.date :start_date
       t.date :end_date
       t.string :coordinator, :null => false
