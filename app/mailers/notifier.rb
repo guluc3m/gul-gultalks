7a0d10b14aef0c049a35ca16434d272798ae27af
Miguel Vaello <miguel.vaellomartinez@gmail.com>
2014-07-14 22:37:06 +0200

    More mailer tweaks, still does not work properly

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,15 +1,17 @@
 class Notifier < ActionMailer::Base
-  default from: "no-reply@example.com" #TOFIX: {DOMAIN ENV}
+  default from: 'no-reply@example.com' #TOFIX: {DOMAIN_ENV}
 
-  def confirmation_vote(recipient)
-
-      mail(to: recipient.speaker_contact_info, subject: '[GUL] Valida tu voto')           
+  def confirmation_vote(talk, key)
+      @talk = talk
+      @key  = key
+      mail(to: @talk.email, subject: '[GUL-TALKS] Valida tu voto')
 
   end
 
-  def confirmation_talk(recipient)
-      mail(to: recipient.speaker_contact_info, subject: '[GUL] Gracias por proponer tu charla -')           
+  def confirmation_talk(talk, key)
+      @talk = talk
+      @key  = key
+      mail(to: @talk.speaker_contact_info, subject: '[GUL-TALKS] Gracias por proponer tu charla')
   end
-
 
 end
