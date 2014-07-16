52882cee409f943c53d581d7215b120d7bd639d9
Miguel Vaello <miguel.vaellomartinez@gmail.com>
2014-07-16 03:01:18 +0200

    Cleanup and refactoring

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,16 +1,16 @@
 class Notifier < ActionMailer::Base
   default from: 'no-reply@example.com' #TOFIX: {DOMAIN_ENV}
 
-  def confirmation_vote(event, key)
+  def confirmation_vote(event, token)
       @event = event
-      @key  = key
+      @token  = token
       mail(to: @talk.email, subject: '[GUL-TALKS] Valida tu voto')
 
   end
 
-  def confirmation_event(event, key)
+  def confirmation_event(event, token)
       @event = event
-      @key  = key
+      @token  = token
       mail(to: @talk.speaker_contact_info, subject: '[GUL-TALKS] Gracias por proponer tu charla')
   end
 
