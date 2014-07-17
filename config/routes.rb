1039ec5fa34aab203815cbb9dae5da7934947de1
Miguel Vaello <miguel.vaellomartinez@gmail.com>
2014-07-17 21:37:16 +0200

    Added Activeadmin routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Gultalks::Application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   resources :conferences do
     resources :events do
       member do
