c4dc79724c6d20a4d2564141d90d08af793a43e5
Miguel Vaello <miguel.vaellomartinez@gmail.com>
2014-07-19 14:04:02 +0200

    Refactoring  mailer

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -4,14 +4,14 @@ class Notifier < ActionMailer::Base
   def confirmation_vote(event, token)
       @event = event
       @token  = token
-      mail(to: @talk.email, subject: '[GUL-TALKS] Valida tu voto')
+      mail(to: @event.email, subject: '[GUL-TALKS] Valida tu voto')
 
   end
 
   def confirmation_event(event, token)
       @event = event
       @token  = token
-      mail(to: @talk.speaker_contact_info, subject: '[GUL-TALKS] Gracias por proponer tu charla')
+      mail(to: @event.speaker_contact_info, subject: '[GUL-TALKS] Gracias por proponer tu charla')
   end
 
 end
