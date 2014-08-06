105d2d423239e7403939f48d05f49197ad46e90d
Rafael Medina <rafamedgar@gmail.com>
2014-08-06 11:48:22 +0200

    Fixed ActiveAdmin not working with gem 'will_paginate', generate slugs automatically if the :slug attribute is left empty (ActiveAdmin)

--- app/models/conference.rb
+++ app/models/conference.rb
@@ -1,8 +1,13 @@
 class Conference < ActiveRecord::Base
   extend FriendlyId
-  attr_accessible :active, :call_for_papers_enabled, :call_for_papers_end_date, :call_for_papers_start_date, :coordinator, :description, :end_date, :location, :start_date, :title, :voting_enabled, :voting_end_date, :voting_start_date
+  attr_accessible :active, :call_for_papers_enabled, :call_for_papers_end_date, :call_for_papers_start_date, :coordinator, :description, :end_date, :location, :slug, :start_date, :title, :voting_enabled, :voting_end_date, :voting_start_date
   has_many :events
   validates_presence_of :title, :description
   validates_uniqueness_of :title
   friendly_id :title, use: :slugged
+
+  private
+  def should_generate_new_friendly_id?
+    slug.blank? || title_changed?
+  end
 end
