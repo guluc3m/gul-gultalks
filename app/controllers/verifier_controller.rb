37cbf5300d604df066985f2b1a49f9d1a9e48e75
Rafael Medina <rafamedgar@gmail.com>
2014-08-20 17:34:55 +0200

    Added basic views for verifiers

--- app/controllers/verifier_controller.rb
+++ app/controllers/verifier_controller.rb
@@ -4,7 +4,7 @@ class VerifierController < ApplicationController
     verifier = Verifier.where(email: params[:email], token: params[:token], event_id: params[:event_id].to_i, verify_type: params[:verify_type]).first
 
     if verifier.nil? || verifier.verified
-      # TODO: render error view
+      render "error"
     else
         event = Event.find(verifier.event_id)
 
@@ -12,11 +12,13 @@ class VerifierController < ApplicationController
           # Validate the event
           event.update_attribute(:active, true)
           verifier.update_attribute(:verified, true)
+          render "event_verified"
 
         elsif verifier.verify_type.eql? "vote"
           # Validate the vote
           event.update_attribute(:votes, votes+1)
           verifier.update_attribute(:verified, true)
+          render "vote_verified"
         end
     end
   end
