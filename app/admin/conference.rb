286062c21cb961071f19b58dba723481c5f6fa23
Rafael Medina <rafamedgar@gmail.com>
2014-08-20 11:05:47 +0200

    Fixed small issue that could occur when using localization between ' '

--- app/admin/conference.rb
+++ app/admin/conference.rb
@@ -22,19 +22,19 @@ ActiveAdmin.register Conference do
 
   form do |f|
     f.inputs do
-      f.input :title, label: t('conference.title')
-      f.input :description, label: t('conference.description')
-      f.input :location, label: t('location')
-      f.input :start_date, label: t('conference.start_date')
-      f.input :end_date, label: t('conference.end_date')
-      f.input :coordinator, label: t('conference.coordinator')
-      f.input :call_for_papers_start_date, label: t('conference.call_for_events_start_date')
-      f.input :call_for_papers_end_date, label: t('conference.call_for_events_end_date')
-      f.input :voting_start_date, label: t('conference.voting_start_date')
-      f.input :voting_end_date, label: t('conference.voting_end_date')
-      f.input :active, label: t('conference.status.active'), as: :radio
-      f.input :call_for_papers_enabled, label: t('conference.status.call_for_events_enabled'), as: :radio
-      f.input :voting_enabled, label: t('conference.status.voting_enabled'), as: :radio
+      f.input :title, label: t("conference.title")
+      f.input :description, label: t("conference.description")
+      f.input :location, label: t("location")
+      f.input :start_date, label: t("conference.start_date")
+      f.input :end_date, label: t("conference.end_date")
+      f.input :coordinator, label: t("conference.coordinator")
+      f.input :call_for_papers_start_date, label: t("conference.call_for_events_start_date")
+      f.input :call_for_papers_end_date, label: t("conference.call_for_events_end_date")
+      f.input :voting_start_date, label: t("conference.voting_start_date")
+      f.input :voting_end_date, label: t("conference.voting_end_date")
+      f.input :active, label: t("conference.status.active"), as: :radio
+      f.input :call_for_papers_enabled, label: t("conference.status.call_for_events_enabled"), as: :radio
+      f.input :voting_enabled, label: t("conference.status.voting_enabled"), as: :radio
       f.input :slug, label: "Slug"
     end
     f.actions
--- app/admin/event.rb
+++ app/admin/event.rb
@@ -25,29 +25,29 @@ ActiveAdmin.register Event do
 
   form do |f|
     f.inputs "Info" do
-      f.input :conference_id, label: t('Conference'), as: :select, collection: Conference.all
-      f.input :title, label: t('event.title')
-      f.input :brief_description, label: t('event.brief_description')
-      f.input :description, label: t('event.description') 
-      f.input :subclass, label: t('event.subclass'), as: :radio, collection: Event.subclasses.keys.collect {|s| [t('event.subclasses.#{s}').humanize, s]}
-      f.input :level, label: t('event.subclass'), as: :radio, collection: Event.levels.keys.collect {|l| [t('event.levels.#{l}').humanize, l]}
-      f.input :speaker, label: t('event.speaker')
-      f.input :email, label: t('event.contact')
-      f.input :content_url, label: t('event.url')
-      f.input :notes, label: t('event.comments')
-      f.input :votes, label: t('votes')
+      f.input :conference_id, label: t("Conference"), as: :select, collection: Conference.all
+      f.input :title, label: t("event.title")
+      f.input :brief_description, label: t("event.brief_description")
+      f.input :description, label: t("event.description") 
+      f.input :subclass, label: t("event.subclass"), as: :radio, collection: Event.subclasses.keys.collect {|s| [t("event.subclasses.#{s}").humanize, s]}
+      f.input :level, label: t("event.subclass"), as: :radio, collection: Event.levels.keys.collect {|l| [t("event.levels.#{l}").humanize, l]}
+      f.input :speaker, label: t("event.speaker")
+      f.input :email, label: t("event.contact")
+      f.input :content_url, label: t("event.url")
+      f.input :notes, label: t("event.comments")
+      f.input :votes, label: t("votes")
       f.input :slug, label: "Slug"
     end
     f.inputs "Organization" do
-      f.input :active, label: t('event.status.active'), as: :radio
-      f.input :cancelled, label: t('event.status.cancelled'), as: :radio
-      f.input :accepted, label: t('event.status.accepted'), as: :radio
-      f.input :location, label: t('location')
-      f.input :room, label: t('event.room')
-      f.input :date, label: t('date')
-      f.input :start_time, label: t('event.start_time')
-      f.input :end_time, label: t('event.end_time')
-      f.input :assisted_by, label: t('event.assistant')
+      f.input :active, label: t("event.status.active"), as: :radio
+      f.input :cancelled, label: t("event.status.cancelled"), as: :radio
+      f.input :accepted, label: t("event.status.accepted"), as: :radio
+      f.input :location, label: t("location")
+      f.input :room, label: t("event.room")
+      f.input :date, label: t("date")
+      f.input :start_time, label: t("event.start_time")
+      f.input :end_time, label: t("event.end_time")
+      f.input :assisted_by, label: t("event.assistant")
     end
     f.actions
   end
