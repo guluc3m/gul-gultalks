b59ded8cfb2bbc051dc893745d2893e53294d2a1
Miguel Vaello <miguel.vaellomartinez@gmail.com>
2014-09-07 02:19:42 +0200

    Updated admin
    - Updated locales
    - Clean up

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,11 +3,11 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      # span class: "blank_slate" do
-      #   span I18n.t("active_admin.dashboard_welcome.welcome")
-      #   small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      # end
+    # div class: "blank_slate_container", id: "dashboard_default_message" do
+    def subclass_count(subclass)
+        Event.where(subclass: subclass, verified: true).order('created_at DESC').limit(8).count
+    end
+
     columns do
       column do
         panel "Recent events" do
@@ -20,9 +20,6 @@ ActiveAdmin.register_page "Dashboard" do
             column(t "event.date") {|event| event.created_at.to_s(:short)}
           end
 
-          def subclass_count(subclass)
-              Event.where(subclass: subclass, verified: true).order('created_at DESC').limit(8).count
-          end
           para "Hay #{t("event.subclasses.talk")} #{subclass_count("talk")}" + " y " + "#{subclass_count("workshop")} #{t("event.subclasses.workshop")}" 
           # .to_sentence(:two_words_connector => ' through ')
         end
@@ -57,5 +54,5 @@ ActiveAdmin.register_page "Dashboard" do
     #     end
     #   end
     # end
-  end # content
+  # end # content
 end
--- app/admin/event.rb
+++ app/admin/event.rb
@@ -38,20 +38,23 @@ ActiveAdmin.register Event do
       f.input :level, label: t("event.subclass"), as: :select, collection: Event.levels.keys.collect {|l| [t("event.levels.#{l}").humanize, l]}
       f.input :duration, label: t("event.duration"), as: :select, collection: Event.durations.keys.collect {|d| [t("event.durations.#{d}").humanize, d]}
       f.input :content_url, label: t("event.url")
+      f.input :code, label: t("event.code").humanize
+      f.input :video, label: t("event.video")
+      f.input :live_video, label: t("event.live_video")
       f.input :notes, label: t("event.comments")
-      f.input :votes, label: t("votes")
+      f.input :votes, label: t("event.votes")
       f.input :slug, label: "Slug"
     end
     f.inputs "Organization" do
       f.input :shown, label: t("event.status.shown"), as: :select
-      f.input :verified, label: t("event.status.verified"), as: :radio
-      f.input :cancelled, label: t("event.status.cancelled"), as: :radio
-      f.input :accepted, label: t("event.status.accepted"), as: :radio
+      f.input :verified, label: t("event.status.verified"), as: :select
+      f.input :cancelled, label: t("event.status.cancelled"), as: :select
+      f.input :accepted, label: t("event.status.accepted"), as: :select
       f.input :location, label: t("location")
       f.input :room, label: t("event.room")
       f.input :start_dtime, label: t("event.start_datetime")
       f.input :end_dtime, label: t("event.end_datetime")
-      f.input :assisted_by, label: t("event.assistant")
+      f.input :assisted_by, label: t("event.assisted_by")
     end
     f.actions
   end
