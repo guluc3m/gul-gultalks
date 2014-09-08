21f50e264aea670edaa7d6baf1bee93e99afdb32
Rafael Medina <rafamedgar@gmail.com>
2014-09-08 21:38:25 +0200

    Fix token size in database

--- db/migrate/20140820111218_create_verifiers.rb
+++ db/migrate/20140820111218_create_verifiers.rb
@@ -4,7 +4,7 @@ class CreateVerifiers < ActiveRecord::Migration
 
       t.string :email, null: false
       t.integer :event_id, null: false
-      t.string :token, limit: 32, null: false
+      t.string :token, limit: 50, null: false
       t.boolean :verified, default: false, null: false
       t.string :verify_type, null: false
 
