9a6ec8e617a4d3593a5d5bbfdef96b026fb489f8
Miguel Vaello Martínez <miguel.vaellomartinez@gmail.com>
2016-08-20 02:43:39 +0200

    Remove some code in order to prevent some conflicts
    TODO: use a helper instead.

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,10 +3,6 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    # div class: "blank_slate_container", id: "dashboard_default_message" do
-    def subclass_count(subclass)
-        Event.where(subclass: subclass, verified: true).order('created_at DESC').limit(8).count
-    end
 
     columns do
       column do
@@ -21,8 +17,6 @@ ActiveAdmin.register_page "Dashboard" do
             column(t "event.date") {|event| event.created_at.to_s(:short)}
           end
 
-          para "Hay #{t("event.subclasses.talk")} #{subclass_count("talk")}" + " y " + "#{subclass_count("workshop")} #{t("event.subclasses.workshop")}"
-          # .to_sentence(:two_words_connector => ' through ')
         end
       end
 
@@ -40,26 +34,4 @@ ActiveAdmin.register_page "Dashboard" do
 
     end
     end
-
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  # end # content
 end
