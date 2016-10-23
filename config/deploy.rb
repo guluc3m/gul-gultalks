b63ef1afc5a571169b72868d8e80d240ac33648b
Miguel Vaello Martínez <miguel.vaellomartinez@gmail.com>
2016-10-23 23:36:36 +0200

    Updated ruby version needed the deploy machine
    - Set 2.3.1 as default

--- config/deploy.rb
+++ config/deploy.rb
@@ -52,7 +52,7 @@ task :environment do
 
   # For those using RVM, use this to load an RVM version@gemset.
   # invoke :'rvm:use[ruby-1.9.3-p125@default]'
-  invoke :'rvm:use[ruby-2.2.1]'
+  invoke :'rvm:use[ruby-2.3.1]'
 end
 
 # Put any custom mkdir's in here for when `mina setup` is ran.
