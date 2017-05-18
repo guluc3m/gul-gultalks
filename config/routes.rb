7a08e8fbe66e6b74a5ea29f6988d5a99661537f2
Rafael Medina <rafamedgar@gmail.com>
2017-05-18 20:04:17 +0100

    Get rid of unsued comment in routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Gultalks::Application.routes.draw do
 
   devise_for :admin
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  # devise_for :admin_users, ActiveAdmin::Devise.config
 
   # Admin
   match 'admin', to: 'admin#index', via: :get, path: '/admin'
