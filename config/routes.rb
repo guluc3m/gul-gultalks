8f392b06c44e9761bb03d5ffd340c317a4c3eb8f
Javier Honduvilla Coto <javierhonduco@gmail.com>
2017-05-19 22:52:16 +0200

    [wip] change routes.rb class name

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
-Gultalks::Application.routes.draw do
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+Rails.application.routes.draw do
 
   devise_for :admin
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
