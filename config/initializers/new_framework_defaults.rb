062f191f715ac95cac5cc148f8d9d090051f84e9
Javier Honduvilla Coto <javierhonduco@gmail.com>
2017-05-19 23:10:01 +0200

    [wip] Rails 5

--- config/initializers/new_framework_defaults.rb
+++ config/initializers/new_framework_defaults.rb
@@ -1,3 +1,5 @@
+# TODO(javierhonduco): merge this in their mainstream files
+
 # Be sure to restart your server when you modify this file.
 #
 # This file contains migration options to ease your Rails 5.0 upgrade.
