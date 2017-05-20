d3c84ecf7abf57a064b365e9fd16ba0b97beca8d
Javier Honduvilla Coto <javierhonduco@gmail.com>
2017-05-20 16:00:01 +0200

    remove 'assigns' from tests as it has been removed from Rails

--- test/functional/conferences_controller_test.rb
+++ test/functional/conferences_controller_test.rb
@@ -4,14 +4,11 @@ class ConferencesControllerTest < ActionController::TestCase
   test "should get conferences" do
     get :index
     assert_response :success
-    assert_not_nil assigns(:conferences)
   end
 
   test "should get conference by ID" do
     conference = conferences(:one)
     get(:show, { 'id' => conference.id})
     assert_response :success
-    assert_not_nil assigns(:conference)
   end
-
 end
